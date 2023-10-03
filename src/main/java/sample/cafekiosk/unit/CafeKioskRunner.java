a9c4ced0fbd6b946bfd3e028acbd37991a9ae104
수지니 <sssssjin99@naver.com>
2023-10-03 22:00:15 +0900

    [feat]: 테스트 케이스 세분화

--- src/main/java/sample/cafekiosk/unit/CafeKioskRunner.java
+++ src/main/java/sample/cafekiosk/unit/CafeKioskRunner.java
@@ -8,10 +8,10 @@ public class CafeKioskRunner {
     public static void main(String[] args) {
         CafeKiosk cafeKiosk = new CafeKiosk();
 
-        cafeKiosk.add(new Americano());
+        cafeKiosk.add(new Americano(), 1);
         System.out.println(">>> 아메리카노 추가");
 
-        cafeKiosk.add(new Latte());
+        cafeKiosk.add(new Latte(), 1);
         System.out.println(">>> 라떼 추가");
 
         int totalPrice = cafeKiosk.calculateTotalPrice();
--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -2,8 +2,10 @@ package sample.cafekiosk.unit;
 
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
+import sample.cafekiosk.unit.beverage.Latte;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CafeKioskTest {
@@ -11,9 +13,50 @@ class CafeKioskTest {
     @Test
     void add() {
         CafeKiosk cafeKiosk = new CafeKiosk();
-        cafeKiosk.add(new Americano());
+        cafeKiosk.add(new Americano(), 1);
 
         assertThat(cafeKiosk.getBeverages()).hasSize(1);
+        assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void addSeveralBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano, 2);
+
+        assertThat(cafeKiosk.getBeverages()).hasSize(2);
+        assertThat(cafeKiosk.getBeverages().get(0)).isEqualTo(americano);
+
+    }
+
+    @Test
+    void addSeveralBeveragesException() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        assertThatThrownBy(() -> cafeKiosk.add(americano, 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("음료는 1잔 이상 주문하실 수 있습니다.");
+    }
+
+    @Test
+    void remove() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        cafeKiosk.add(americano, 1);
+        cafeKiosk.remove(americano);
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
+    }
+
+    @Test
+    void clear() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano(), 1);
+        cafeKiosk.add(new Latte(), 1);
+        cafeKiosk.clear();
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
     }
 
 }

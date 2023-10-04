3b5243dbb4afb50e4af226ee09e1e55095a7761f
수지니 <sssssjin99@naver.com>
2023-10-04 17:22:47 +0900

    [feat]: Business Layer 테스트 1

--- src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
@@ -15,4 +15,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
      */
     List<Product> findAllBySellingStatusIn(List<ProductSellingStatus> sellingTypes);
 
+    List<Product> findAllByProductNumberIn(List<String> productNumbers);
+
 }
